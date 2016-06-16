74e24981c31445a16734d921ef4781dab8070d3a
Mike Jones <mike@westerncapelabs.com>
2016-06-16 02:22:50 +0200

    Add static paths

--- uopbmoh_hub/urls.py
+++ uopbmoh_hub/urls.py
@@ -1,6 +1,7 @@
 import os
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.site.site_header = os.environ.get('UOPBMOH_HUB_TITLE', 'UoPBMoH Admin')
 
@@ -10,3 +11,4 @@ urlpatterns = patterns(
     url(r'^admin/',  include(admin.site.urls)),
     url(r'^', include('hub.urls')),
 )
+urlpatterns += staticfiles_urlpatterns()
